2c6e4819d25d76b72a3536c50e2b5f30d3ed34bd
MartinNose <liucmartin@qq.com>
2020-07-16 21:25:54 +0800

    lab1 commented

--- lab1.sh
+++ lab1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 判断输入是否存在以及是否是目录
 if [ -e "$1" ]
 then
     if [ -d "$1" ]
@@ -14,31 +15,35 @@ else
     exit 1
 fi
 
-fcnt=0
-xcnt=0
-dcnt=0
-ccnt=0
+fcnt=0  # 普通文件数量
+xcnt=0  # 可执行文件数量
+dcnt=0  # 目录文件数量
+ccnt=0  # 普通文件字节数
 
+# 遍历输入目录下所有文件
 for file in "$1"/*
 do
-    if [ -r "$file" ]
+    if [ -r "$file" ] # 判断是否可读
     then
-        if [ -d "$file" ]
+        if [ -d "$file" ]       # 如果是目录，目录文件计数加一
         then
             dcnt=$((dcnt + 1))
-            ccnt=$((ccnt + tmp))
-        elif [ -x "$file" ]
+        elif [ -x "$file" ]     # 如果是可执行文件，相关计数加一
         then 
             xcnt=$((xcnt + 1))
-        elif [ -f "$file" ]
+        elif [ -f "$file" ]     # 如果是普通文件，则文件计数加一，统计文件字符数并累加入相关变量
         then
             fcnt=$((fcnt + 1))
             tmp=$(wc -c < "$file")
+            ccnt=$((ccnt + tmp))
        fi
     else
         echo "$file is not readable."
     fi
 done   
+
+# 输出结果
 echo "There are $fcnt file(s), $dcnt directory(ies) and $xcnt excutable file(s) in directory '$1'."
 echo "$ccnt character(s) in total."
+
 exit 0
